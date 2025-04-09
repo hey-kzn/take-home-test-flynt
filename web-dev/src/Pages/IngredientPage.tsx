a0af82e75e808d02fa5adef9518acb375558eee0
KZN <45941342+hey-kzn@users.noreply.github.com>
2025-04-09 20:40:45 +0200

    fix ingredients list

--- web-dev/src/Pages/IngredientPage.tsx
+++ web-dev/src/Pages/IngredientPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { Box, Button } from "@mui/material";
 import { useQueryIngredientList } from "../Hooks/Query/IngredientQuery";
 import { Loader } from "../Components/Loader";
